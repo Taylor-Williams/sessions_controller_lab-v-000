1ae11efae68164497152f3b1b05938c2de6c6fc0
Taylor Williams <taylor@thewilliams.net>
2019-01-02 20:08:15 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     end
     if params[:name] && params[:name] != ''
       session[:username] = params[:name]
-      redirect_to '/login'
+      redirect_to '/'
     else
       redirect_to '/login'
     end
